12730551f8f3f0315c57967d464d16f0a4453b72
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-07-17 19:42:57 +0900

    update dependencies and devDependencies
    Highlight: update @shinnn/eslint-config and follow its rules, for exmaple tab indent

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const path = require('path');
 const inspectWithKind = require('inspect-with-kind');
 
 function isDirLikePathCore(filePath, {normalize, sep}) {
-  if (typeof filePath !== 'string') {
-    throw new TypeError(`Expected a file path, but got ${inspectWithKind(filePath)}.`);
-  }
+	if (typeof filePath !== 'string') {
+		throw new TypeError(`Expected a file path, but got ${inspectWithKind(filePath)}.`);
+	}
 
-  return normalize(filePath).endsWith(sep);
+	return normalize(filePath).endsWith(sep);
 }
 
 module.exports = function isDirLikePath(filePath) {
-  return isDirLikePathCore(filePath, path);
+	return isDirLikePathCore(filePath, path);
 };
 
 module.exports.posix = function isDirLikePathPosix(filePath) {
-  return isDirLikePathCore(filePath, path.posix);
+	return isDirLikePathCore(filePath, path.posix);
 };
 
 module.exports.win32 = function isDirLikePathWin32(filePath) {
-  return isDirLikePathCore(filePath, path.win32);
+	return isDirLikePathCore(filePath, path.win32);
 };
--- test.js
+++ test.js
@@ -6,55 +6,55 @@ const isDirLikePath = require('.');
 const tap = require('tap');
 
 tap.test('isDirPathPath', t => {
-  t.equal(
-    isDirLikePath('foo/'),
-    true,
-    'should return `true` if the string looks like a directory path.'
-  );
+	t.equal(
+		isDirLikePath('foo/'),
+		true,
+		'should return `true` if the string looks like a directory path.'
+	);
 
-  t.equal(
-    isDirLikePath(resolve('/0', '1/2/3/4.txt')),
-    false,
-    'should support absolute path.'
-  );
+	t.equal(
+		isDirLikePath(resolve('/0', '1/2/3/4.txt')),
+		false,
+		'should support absolute path.'
+	);
 
-  t.equal(
-    isDirLikePath(''),
-    false,
-    'should return `false` if it takes an empty string.'
-  );
+	t.equal(
+		isDirLikePath(''),
+		false,
+		'should return `false` if it takes an empty string.'
+	);
 
-  t.throws(
-    () => isDirLikePath(Buffer.from('a')),
-    new TypeError('Expected a file path, but got <Buffer 61>.'),
-    'should throw a type error when it takes a non-string argument.'
-  );
+	t.throws(
+		() => isDirLikePath(Buffer.from('a')),
+		new TypeError('Expected a file path, but got <Buffer 61>.'),
+		'should throw a type error when it takes a non-string argument.'
+	);
 
-  t.throws(
-    () => isDirLikePath(),
-    new TypeError('Expected a file path, but got undefined.'),
-    'should throw a type error when it takes no arguments.'
-  );
+	t.throws(
+		() => isDirLikePath(),
+		new TypeError('Expected a file path, but got undefined.'),
+		'should throw a type error when it takes no arguments.'
+	);
 
-  t.end();
+	t.end();
 });
 
 tap.test('isDirLikePath.posix', t => {
-  t.strictEqual(
-    isDirLikePath.posix('1\\2\\'),
-    false,
-    'should always treat the path in a posix compatible way.'
-  );
+	t.strictEqual(
+		isDirLikePath.posix('1\\2\\'),
+		false,
+		'should always treat the path in a posix compatible way.'
+	);
 
-  t.end();
+	t.end();
 });
 
 tap.test('isDirLikePath.win32', t => {
-  t.equal(
-    isDirLikePath.win32('1\\2\\'),
-    true,
-    'should always treat the path in a win32 compatible way.'
-  );
+	t.equal(
+		isDirLikePath.win32('1\\2\\'),
+		true,
+		'should always treat the path in a win32 compatible way.'
+	);
 
-  t.end();
+	t.end();
 });
